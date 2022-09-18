7e6ee2b585b183addd7a81f18d2f50c2de3fd017
kms1212 <kms1212g@gmail.com>
2022-09-18 19:56:26 +0900

    Some basic codes

--- Sources/LoadableValue/LoadableValue.swift
+++ Sources/LoadableValue/LoadableValue.swift
@@ -1,6 +1,75 @@
-public struct LoadableValue {
-    public private(set) var text = "Hello, World!"
+//
+//  LoadableValue.swift
+//  iosApp
+//
+//  Created by 권민수 on 2022/08/10.
+//
 
-    public init() {
+import Foundation
+
+enum LoadableValue<T, E: Error> {
+    case success(T)
+    case failure(E)
+    case loading
+    case waiting
+}
+
+extension LoadableValue {
+    init(_ from: Result<T, E>) {
+        switch from {
+        case .success(let val):
+            self = .success(val)
+        case .failure(let err):
+            self = .failure(err)
+        }
+    }
+
+    init() {
+        self = .waiting
+    }
+
+    var isSuccess: Bool {
+        switch self {
+        case .success:
+            return true
+        default:
+            return false
+        }
+    }
+
+    var isFailure: Bool {
+        switch self {
+        case .failure:
+            return true
+        default:
+            return false
+        }
+    }
+
+    var isLoading: Bool {
+        switch self {
+        case .loading:
+            return true
+        default:
+            return false
+        }
+    }
+
+    var isWaiting: Bool {
+        switch self {
+        case .waiting:
+            return true
+        default:
+            return false
+        }
+    }
+
+    var getData: T? {
+        switch self {
+        case .success(let data):
+            return data
+        default:
+            return nil
+        }
     }
 }
--- Tests/LoadableValueTests/LoadableValueTests.swift
+++ Tests/LoadableValueTests/LoadableValueTests.swift
@@ -1,11 +1,59 @@
 import XCTest
 @testable import LoadableValue
 
+
+enum SampleError: Error {
+    case sampleError
+}
+
+extension SampleError: LocalizedError {
+    public var errorDescription: String? {
+        switch self {
+        case .sampleError:
+            return "Cookie not found."
+        }
+    }
+}
+
 final class LoadableValueTests: XCTestCase {
     func testExample() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(LoadableValue().text, "Hello, World!")
+        var testVariable = LoadableValue<Int, Error>()
+
+        guard case .waiting = testVariable else {
+            XCTFail("Test Failed.")
+            return
+        }
+        XCTAssertTrue(testVariable.isWaiting)
+        XCTAssertEqual(testVariable.getData, nil)
+
+        testVariable = .loading
+
+        guard case .loading = testVariable else {
+            XCTFail("Test Failed.")
+            return
+        }
+        XCTAssertTrue(testVariable.isLoading)
+        XCTAssertEqual(testVariable.getData, nil)
+
+        testVariable = .failure(SampleError.sampleError)
+
+        guard case .failure(SampleError.sampleError) = testVariable else {
+            XCTFail("Test Failed.")
+            return
+        }
+        XCTAssertTrue(testVariable.isFailure)
+        XCTAssertEqual(testVariable.getData, nil)
+
+        testVariable = .success(1)
+
+        guard case .success(1) = testVariable else {
+            XCTFail("Test Failed.")
+            return
+        }
+        XCTAssertTrue(testVariable.isSuccess)
+        XCTAssertEqual(testVariable.getData!, 1)
     }
 }
